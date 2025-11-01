8aeca5d1777841eb3919fdfaa45c6c29f4b5cb3e
lexflame <lexflame@gmail.com>
2025-11-01 23:15:00 +0700

    Remastering

--- agents/task_agent/classes/item_task.py
+++ agents/task_agent/classes/item_task.py
@@ -7,6 +7,7 @@ from PyQt6.QtCore import Qt, QSize, QPropertyAnimation, QEasingCurve
 
 class ItemTask(QWidget):
     def __init__(self, title, priority, deadline, list_item=None, complite_event=None, delete_event=None, data=None):
+
         super().__init__()
         self.is_complited_task = None
         self._list_item = list_item
@@ -16,11 +17,9 @@ class ItemTask(QWidget):
         self.setStyleSheet(f"""QWidget {{color: #ddd;background-color: transparent !important;}}""")
 
     def setupUi(self, title, priority, deadline, data):
-
         self.box_item_layout = QHBoxLayout(self)
-        self.box_item_layout.setContentsMargins(10, 10, 10, 10)  # Увеличили отступы
+        self.box_item_layout.setContentsMargins(10, 10, 10, 10) # Увеличили отступы
         self.box_item_layout.setSpacing(2)
-
         """ Область Drag&Drop === start """
         drag_label = QLabel()
         drag_label.setStyleSheet("border:1px solid red;")
@@ -30,19 +29,32 @@ class ItemTask(QWidget):
         btn_check.setIcon(icon)
         btn_check.setIconSize(QSize(24, 24))
         btn_check.setFixedSize(25, 25)
-        btn_check.setToolTip('Выполнить')  # подсказка при наведении
+        btn_check.setToolTip('Выполнить') # подсказка при наведении
         # Привязка клика к callback (если он передан)
         if self._complite_event is not None and self._list_item is not None:
             # передаем QListWidgetItem в контроллер
             btn_check.clicked.connect(lambda: self._complite_event(self._list_item))
-        self.box_item_layout.addWidget(drag_label, stretch=1)
+            self.box_item_layout.addWidget(drag_label, stretch=1)
         """ Область Drag&Drop === end """
 
+        """ Область Data View === start """
         header_label = QLabel()
         header_label.setStyleSheet("border:1px solid white;")
         self.box_item_layout.addWidget(header_label, stretch=25)
+        """ Область Data View === end """
 
-        action_label = QLabel()
-        action_label.setStyleSheet("border:1px solid blue;")
-        self.box_item_layout.addWidget(action_label, stretch=4)
+        """ Область Action === end """
+        self.action_label = QLabel()
+        self.action_label.setStyleSheet("border:1px solid blue;")
+        self.box_item_layout.addWidget(self.action_label, stretch=4)
+        """ Область Action === start """
+
+        """ Область Spoiler Desc === end """
+        desc_label = QLabel()
+        desc_label.setStyleSheet("border:1px solid blue;")
+        self.box_item_layout.addWidget(desc_label, stretch=1)
+        """ Область Spoiler Desc === start """
+
+
+
 
